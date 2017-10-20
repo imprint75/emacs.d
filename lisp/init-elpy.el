7c059574113e6db13a1691cfb0378bde9092d9c3
imprint75 <imprint75@gmail.com>
2017-10-20 15:26:40 -0400

    sets elpy rpc backend to jedi

--- lisp/init-elpy.el
+++ lisp/init-elpy.el
@@ -2,6 +2,7 @@
 ;;; https://github.com/jorgenschaefer/elpy
 (package-initialize)
 (elpy-enable)
+(setq elpy-rpc-backend "jedi")
 (define-key elpy-mode-map (kbd "M-<left>") nil)
 (define-key elpy-mode-map (kbd "M-<right>") nil)
 (define-key elpy-mode-map (kbd "C-c C-d") nil)
